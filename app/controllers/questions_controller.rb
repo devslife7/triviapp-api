c3d8b9d536ca7a8cb072ea9b23d87af266e6f757
andy-aguilar <aaguil3@gmail.com>
2020-08-17 15:04:44 -0400

    fixed tokens for JWT Auth

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,4 @@
 class QuestionsController < ApplicationController
-    skip_before_action :authorized, only: [:index]
 
     def index
         questions = Question.all.sample(10)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            render json: { user: UserSerializer.new(user) }, status: :created
+            token = encode_token({ user_id: user.id })
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
