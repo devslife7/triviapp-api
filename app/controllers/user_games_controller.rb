89b67a2f188804fe49356d0ea1ac1ecd86efb8cd
andy-aguilar <aaguil3@gmail.com>
2020-08-19 11:41:30 -0400

    fixed join game backend

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,10 +1,16 @@
 class UserGamesController < ApplicationController
     skip_before_action :authorized, only: [:create, :show, :update]
 
-    def create 
-        game = Game.find_by(name: params[:name])
-        user = User.find_by(username: params[:username])
+    def create
+        
+        game = Game.find_by(name: params[:usergame][:name])
+        user = User.find_by(username: params[:usergame][:username])
         usergame = UserGame.create(game_id: game.id, user_id: user.id, score: 0)
+        if usergame
+            render json: { usergame: usergame }
+        else 
+            render json: { message: "that didn't work" }
+        end
     end
 
     def update
